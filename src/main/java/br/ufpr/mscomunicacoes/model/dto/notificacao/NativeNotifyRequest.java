860e2adbcbaef201481e4b2674bafa6427bb4573
Rafael Dias Simeoni <rafaelsimeoni28@gmail.com>
2023-11-16 22:47:14 -0300

    Correção appID

--- src/main/java/br/ufpr/mscomunicacoes/model/dto/notificacao/NativeNotifyRequest.java
+++ src/main/java/br/ufpr/mscomunicacoes/model/dto/notificacao/NativeNotifyRequest.java
@@ -6,14 +6,14 @@ import lombok.*;
 @Getter
 @Setter
 public class NativeNotifyRequest {
-    private Integer appID;
+    private Integer appId;
     private String appToken;
     private String subID;
     private String title;
     private String message;
 
     public NativeNotifyRequest(String subID, String title, String message) {
-        this.appID = 14520;
+        this.appId = 14520;
         this.appToken = "vdk0Ur8ZprhkWw4MiubMKt";
         this.subID = subID;
         this.title = title;
