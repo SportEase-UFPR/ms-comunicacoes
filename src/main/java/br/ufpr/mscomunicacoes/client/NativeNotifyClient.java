cdac8c8d0300253ef5e330b02354c0f4070357e5
Rafael Dias Simeoni <rafaelsimeoni28@gmail.com>
2023-11-16 23:10:59 -0300

    Notificação push assíncrona

--- src/main/java/br/ufpr/mscomunicacoes/client/NativeNotifyClient.java
+++ src/main/java/br/ufpr/mscomunicacoes/client/NativeNotifyClient.java
@@ -2,16 +2,19 @@ package br.ufpr.mscomunicacoes.client;
 
 
 import br.ufpr.mscomunicacoes.model.dto.notificacao.NativeNotifyRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.concurrent.CompletableFuture;
+
 
 @Service
+@Slf4j
 public class NativeNotifyClient {
 
     @Value("${url.native_notify}")
@@ -31,7 +34,13 @@ public class NativeNotifyClient {
 
     public void fazerRequisicaoNotifyPush(NativeNotifyRequest request) {
         var headers = gerarCabecalho();
-        restTemplate.exchange(urlNativeNotify, HttpMethod.POST, new HttpEntity<>(request, headers), Object.class);
+        CompletableFuture.runAsync(() -> {
+            try {
+                restTemplate.postForEntity(urlNativeNotify, new HttpEntity<>(request, headers), Void.class);
+            } catch (Exception e) {
+                log.error(e.getMessage());
+            }
+        });
     }
 
 
